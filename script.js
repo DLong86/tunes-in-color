f11c62281aaf7addf31c0054f5c41102389404af
DLong <daveallenlong@gmail.com>
2024-03-19 12:19:54 +0900

    carousel changes

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slider img");
+const totalSlides = slides.length;
 
+function showSlide(index) {
+	slides.forEach((slide, i) => {
+		slide.style.transform = `translateX(-${
+			index * 100
+		}%)`; /* Move to the correct position */
+	});
+}
+
+function nextSlide() {
+	currentIndex = (currentIndex + 1) % totalSlides;
+	showSlide(currentIndex);
+}
+
+function prevSlide() {
+	currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+	showSlide(currentIndex);
+}
+
+// Show the first slide initially
+showSlide(currentIndex);
