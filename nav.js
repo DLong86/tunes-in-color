cffae1d7472558b1c370a027296fea7aab5e1cda
DLong86 <daveallenlong@gmail.com>
2024-04-01 15:22:07 +0900

    updated - navigation

--- nav.js
+++ nav.js
@@ -1,15 +1,16 @@
 function toggleNavigation() {
-	const navigation = document.querySelector(".navigation");
-	navigation.classList.toggle("show");
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("show");
 
-	const body = document.querySelector("body");
-	body.classList.toggle("nav-open");
+  const body = document.querySelector("body");
+  body.classList.toggle("nav-open");
 }
+
 // Close navigation when clicking on the main content
 document.querySelector(".main").addEventListener("click", function () {
-	const navigation = document.querySelector(".navigation");
-	navigation.classList.remove("show");
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.remove("show");
 
-	const body = document.querySelector("body");
-	body.classList.remove("nav-open");
+  const body = document.querySelector("body");
+  body.classList.remove("nav-open");
 });
