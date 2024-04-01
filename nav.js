b067daa09782b5d3d6f13a8c5c6d633dd1b69c46
DLong <daveallenlong@gmail.com>
2024-04-01 12:23:40 +0900

    added - navigation

---
+++ nav.js
@@ -0,0 +1,15 @@
+function toggleNavigation() {
+	const navigation = document.querySelector(".navigation");
+	navigation.classList.toggle("show");
+
+	const body = document.querySelector("body");
+	body.classList.toggle("nav-open");
+}
+// Close navigation when clicking on the main content
+document.querySelector(".main").addEventListener("click", function () {
+	const navigation = document.querySelector(".navigation");
+	navigation.classList.remove("show");
+
+	const body = document.querySelector("body");
+	body.classList.remove("nav-open");
+});
